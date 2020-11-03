6a2875ac6c115afd5c6f4e8d3a685c5c995778fe
Christian Muehlhaeuser <muesli@gmail.com>
2020-11-03 16:58:40 +0100

    Brew formula update for markscribe version v0.3.0

--- markscribe.rb
+++ markscribe.rb
@@ -2,12 +2,12 @@
 class Markscribe < Formula
   desc "Your personal markdown scribe with template-engine and Git(Hub) & RSS powers"
   homepage "https://fribbledom.com/"
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/muesli/markscribe/releases/download/v0.2.0/markscribe_0.2.0_Darwin_x86_64.tar.gz"
-    sha256 "31fa2ad532672b1a23c78573b3be953b778ea0c4a9cf96f04b5666ff34126d95"
+    url "https://github.com/muesli/markscribe/releases/download/v0.3.0/markscribe_0.3.0_Darwin_x86_64.tar.gz"
+    sha256 "810ebef6993135c2fc011d4bf96a912975df121d54e61e4ac2259b8f39d11afe"
   elsif OS.linux?
   end
 
