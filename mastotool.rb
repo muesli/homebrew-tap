2d48c8e730ae2581408b41f19301b98785f34398
Christian Muehlhaeuser <muesli@gmail.com>
2020-11-07 06:21:53 +0100

    Brew formula update for mastotool version v0.2.4

--- mastotool.rb
+++ mastotool.rb
@@ -2,24 +2,24 @@
 class Mastotool < Formula
   desc "Mastodon CLI tool & statistics generator"
   homepage "https://fribbledom.com/"
-  version "0.2.3"
+  version "0.2.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/muesli/mastotool/releases/download/v0.2.3/mastotool_0.2.3_Darwin_x86_64.tar.gz"
-    sha256 "199b0084da41b3d97022054ec9a92b50bd4b89acff4eedc06452e5aea72b81a2"
+    url "https://github.com/muesli/mastotool/releases/download/v0.2.4/mastotool_0.2.4_Darwin_x86_64.tar.gz"
+    sha256 "f4a0640460daad9508dfc2beffbed41395470dc54dbade136643a71dc6e7b02e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/muesli/mastotool/releases/download/v0.2.3/mastotool_0.2.3_linux_x86_64.tar.gz"
-      sha256 "e0c5edd3594c2396902587b6b94d496472374d253fbc7e6cf5eb6873aada0154"
+      url "https://github.com/muesli/mastotool/releases/download/v0.2.4/mastotool_0.2.4_linux_x86_64.tar.gz"
+      sha256 "a3f5999002190dd079ea6ac98f77e5555c82177c89b8154c058a381b73fea1f4"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/muesli/mastotool/releases/download/v0.2.3/mastotool_0.2.3_linux_arm64.tar.gz"
-        sha256 "d847f7a14b514f1741aca87d01a18c83171d6dceb50d9d7db0c060a9969ba216"
+        url "https://github.com/muesli/mastotool/releases/download/v0.2.4/mastotool_0.2.4_linux_arm64.tar.gz"
+        sha256 "6191261f199790cdc8fefeba5eccd9b9d5bb4053f275647fdffaddb81fc9d58e"
       else
-        url "https://github.com/muesli/mastotool/releases/download/v0.2.3/mastotool_0.2.3_linux_armv6.tar.gz"
-        sha256 "497676c8f0f74a8930d3db3d9e4e3f70cc6b83a074d4d601e9e0cf7a1478561a"
+        url "https://github.com/muesli/mastotool/releases/download/v0.2.4/mastotool_0.2.4_linux_armv6.tar.gz"
+        sha256 "d8d0bb714e36c09f316d6e6c73aae387981f0fdb6b85468ddc87b8fd738f48d0"
       end
     end
   end
