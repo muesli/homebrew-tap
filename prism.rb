7682f305b88fd825342fe6741cfe0a3d3e0fdbaa
Christian Muehlhaeuser <muesli@gmail.com>
2020-12-23 00:02:50 +0100

    Brew formula update for prism version v0.1.1

--- prism.rb
+++ prism.rb
@@ -2,26 +2,24 @@
 class Prism < Formula
   desc "Disk Usage/Free Utility"
   homepage "https://fribbledom.com/"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/muesli/prism/releases/download/v0.1.0/prism_0.1.0_Darwin_x86_64.tar.gz"
-    sha256 "bad49c43cd1a6c21bc7bb5d38e0470c60e7ac197d0e7437238b2693bccd6813d"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/muesli/prism/releases/download/v0.1.0/prism_0.1.0_linux_x86_64.tar.gz"
-      sha256 "e0b25dd65bf1bbc80a18e9be3f95fe4ad633d373f75321ba86731d7c15d4018b"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/muesli/prism/releases/download/v0.1.0/prism_0.1.0_linux_arm64.tar.gz"
-        sha256 "a4d30463b659569ce35a3a0e635df7c2850b6f353efa943b2a99930593236d74"
-      else
-        url "https://github.com/muesli/prism/releases/download/v0.1.0/prism_0.1.0_linux_armv6.tar.gz"
-        sha256 "e43a66f5545ae7eae8a053d2799ada789d6a627948d8548585f4e9c0f3f99ba5"
-      end
-    end
+    url "https://github.com/muesli/prism/releases/download/v0.1.1/prism_0.1.1_Darwin_x86_64.tar.gz"
+    sha256 "692b515aca5828f8bd5bbb03288812aebb7be3a797686a3edbcb91551a9a40b3"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/muesli/prism/releases/download/v0.1.1/prism_0.1.1_linux_x86_64.tar.gz"
+    sha256 "58f846473ddeef0f0c0c8ecc31ca5a8b3c2d45a417bd93ef6356f7bc1b7e94d8"
+  end
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/muesli/prism/releases/download/v0.1.1/prism_0.1.1_linux_armv6.tar.gz"
+    sha256 "429b1c4317a7f8d9e3677faf0aecb8e126fb4c568e5f4e96f6c69cc4de940725"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/muesli/prism/releases/download/v0.1.1/prism_0.1.1_linux_arm64.tar.gz"
+    sha256 "daff3f189c857dc30398c576c8561d899b18e0ad254497fad120fd725caf5033"
   end
 
   def install
